b6588bca4a38a138c75fdfc4632ca0aba0cef54c
Mdrasheen <138602497+Mdrasheen@users.noreply.github.com>
2025-10-23 13:49:44 +0530

    Eco Performance-bought,spendsaved

--- ecobazzar/src/main/java/com/ecobazzar/ecobazzar/repository/OrderRepository.java
+++ ecobazzar/src/main/java/com/ecobazzar/ecobazzar/repository/OrderRepository.java
@@ -1,13 +1,18 @@
-	package com.ecobazzar.ecobazzar.repository;
+package com.ecobazzar.ecobazzar.repository;
 
 import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ecobazzar.ecobazzar.model.Order;
-
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 	
 	List<Order> findByUserId(Long userId);
+	
+	@Query("SELECT SUM(o.totalPrice) from Order o where o.userId = :userId")
+	Double getTotalSpendByUser(Long userId);
+	
+	
+	@Query("SELECT SUM(o.totalCarbon) from Order o where o.userId = :userId")
+	Double getTotalCarbonByUser(Long userId);
 
 }
